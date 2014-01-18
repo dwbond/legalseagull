71ae656d7c318fab2d4e07bf054511b1631f96ca
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-17 23:14:58 -0500

    Renamed the installed_apps.

--- legalseagull/legalseagull/settings.py
+++ legalseagull/legalseagull/settings.py
@@ -44,8 +44,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'scotus',
-    'uscode',
+    'legislation',
+    'courts',
     'south',
 )
 
