b09673745707ce12792577154a5d490d80cbe548
Daniel W Bond <dbond2@gmu.edu>
2014-01-18 17:07:09 -0500

    things are now happening in the views

--- legalseagull/courts/views.py
+++ legalseagull/courts/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from courts.models import Base, Tags, Justice, Opinion, Case
-from django.contrib.auth.decorators import login_required
 import time
 
-@login_required
 def index(request):
     cases = Case.objects.all()# .order_by('-decisionDate')
     # tags = Tags.objects.all()
@@ -12,30 +10,34 @@ def index(request):
     },
     )
 
-@login_required
 def justice(request, slug):
     justice = get_object_or_404(Justice, slug=slug)
+    cases = Case.objects.all()#.orderby('-decisionDate')
     return render(request, 'justice.html', {
+        'justice' = justice,
+
     },
     )
 
-@login_required
 def tags(request, slug):
-    tags = get_object_or_404(Tags, slug=slug)
+    tag = get_object_or_404(Tags, slug=slug)
+    cases = Case.objects.all()#.order_by('-decisionDate')
     return render(request,'tags.html', {
+        'tag' : tag,
+        'cases' : cases,
     },
     )
 
-@login_required
 def opinion(request, slug):
     opinion = get_object_or_404(Opinion, slug=slug) 
     return render(request, 'opinion.html', {
+        'opinion' : opinion,
     },
     )
 
-@login_required
 def case(request, slug):
-    case = get_object_or_404(Case, slug=slug)
+    opinions = get_object_or_404(Case, slug=slug)
     return render(request, 'case.html', {
+        'opinions' : opinions,
     },
     )
