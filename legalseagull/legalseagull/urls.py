2e91d1524ef95358b32680c0f640653fae4119e0
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-17 23:25:54 -0500

    Correctly delegate per-app urls.

--- legalseagull/legalseagull/urls.py
+++ legalseagull/legalseagull/urls.py
@@ -3,6 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from legalseagull.views import index
+
+
 urlpatterns = patterns('',
+    # LegalSeagull default urls.
+    url(r'^$', index),
+
+    # App sub-urls.
+    url(r'^legislation/', include('legislation.urls')),
+    url(r'^courts/', include('courts.urls')),
+
+    # Admin interface.
     url(r'^admin/', include(admin.site.urls)),
 )
