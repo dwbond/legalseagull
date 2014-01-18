5462f8b68d4930e8fd625807879f462d29ddfbac
Daniel W Bond <dbond2@gmu.edu>
2014-01-18 17:43:12 -0500

    added description field in

--- legalseagull/courts/views.py
+++ legalseagull/courts/views.py
@@ -15,14 +15,14 @@ def justice(request, slug):
     justice = get_object_or_404(Justice, slug=slug)
     cases = Case.objects.all()#.orderby('-decisionDate')
     return render(request, 'justice.html', {
-        'justice' = justice,
+        'justice' : justice,
 
     },
     )
 
 def tags(request, slug):
     tag = get_object_or_404(Tags, slug=slug)
-    cases = Case.objects.all()#.order_by('-decisionDate')
+    cases = Case.objects.filter(tag)#.order_by('-decisionDate')
     return render(request,'tags.html', {
         'tag' : tag,
         'cases' : cases,
