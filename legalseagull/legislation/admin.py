34cf5666aa32fad9bd6ccd0760d8292c86eb9280
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-18 02:09:39 -0500

    Registered legislation models in admin interface.

--- legalseagull/legislation/admin.py
+++ legalseagull/legislation/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from legislation.models import Title, Chapter, Section
 
 # Register your models here.
+admin.site.register( Title )
+admin.site.register( Chapter )
+admin.site.register( Section )
