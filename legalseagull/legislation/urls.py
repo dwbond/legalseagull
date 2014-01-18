f89b67e8798a9b0f09bde9746df9aa0aeac04f54
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-18 02:21:37 -0500

    Added title view page.

--- legalseagull/legislation/urls.py
+++ legalseagull/legislation/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
-
-from legislation.views import index
+from legislation.views import index, view_title
 
 urlpatterns = patterns('',
-    url(r'^$', index),
+    url(r'^$', index, name='legislation_index'),
+    url(r'^(?P<number>\d+)', view_title),
 )
