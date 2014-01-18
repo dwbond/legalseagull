8540ad4c2499449fdde64dcdfb5d9a9e62f28eb0
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-18 09:45:08 -0500

    Registered court models in admin.

--- legalseagull/courts/admin.py
+++ legalseagull/courts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from courts.models import Tags, Justice, Opinion, Case
 
 # Register your models here.
+admin.site.register( Tags )
+admin.site.register( Justice )
+admin.site.register( Opinion )
+admin.site.register( Case )
