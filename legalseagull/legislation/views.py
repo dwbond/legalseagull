4677dd9516207a16a4258a67ac751dd7d0b5c174
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-18 16:18:00 -0500

    Password protected views.

--- legalseagull/legislation/views.py
+++ legalseagull/legislation/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
 from legislation.models import Title
 
 # Create your views here.
+@login_required
 def index(request):
     titles = Title.objects.all()
     return render(request, 'legislation.html', {
@@ -9,6 +11,7 @@ def index(request):
     },
     )
 
+@login_required
 def view_title(request, number):
     title = get_object_or_404(Title, number=number)
     return render(request, 'title.html', {
