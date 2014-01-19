3c3969f11fcd90e14e1a8df95aa7d7759b2c8b52
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-19 07:04:09 -0500

    Added sidebar javascripting.

---
+++ legalseagull/static/js/sidebar.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+
+  // Manage the display and handling of citations.
+  $(".citation-generator").click(function(){
+    $("#citation-container").show(400);
+
+    var source = $(this);
+    var citation = "U.S.C.";
+    citation += source.attr("data-target");
+    $("#citation").text( citation );
+  });
+
+});
