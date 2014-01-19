31e51bf130db511ff07e6ff5a556429ab8e455c2
Daniel W Bond <dbond2@gmu.edu>
2014-01-19 00:21:56 -0500

    describing the battle plan

--- legalseagull/courts/scraping/scriptier.sh
+++ legalseagull/courts/scraping/scriptier.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-for directory in *
+for directory in Abortion
 do
   while read line
   do
     url=( $line )
     filename=$( echo $url | sed 's/\(.*\)\/\(.*\/\)//')
-    wget $url -P "$directory"
     curl $url | sed '0,/^<casecontent>/d' | sed -n -e '0,/<\/casecontent/p' | sed '$d' > "$directory/$filename"
   done < "$directory/contents.html"
 done
