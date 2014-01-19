cf8681b70e5ea0d664b3dd802939df6b67fa5f31
Jean Michel Rouly <michel.rouly@gmail.com>
2014-01-19 07:51:11 -0500

    Made citations more fun.

--- legalseagull/static/js/sidebar.js
+++ legalseagull/static/js/sidebar.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
 
   // Manage the display and handling of citations.
   $(".citation-generator").click(function(){
+    $("#citation-container").hide(400);
     $("#citation-container").show(400);
 
     var source = $(this);
